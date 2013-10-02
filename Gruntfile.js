d1717d76ceb671a52471eafd8a2178fb8fff4f68
Max Fellmuth <max@maxfellmuth.com>
2013-10-02 22:31:25 +0200

    Removed some grunt stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,43 +2,9 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    csslint: {
-      css: {
-        src: ['stylesheets/!(ie).css']
-      },
-      
-    },
-    jekyll: {
-      server: {
-        server : true,
-        server_port : 3000,
-        auto : true
-      },
-      build: {
-        server: false
-      }
-    },
-    //System Configuartion. You sdhouldn't need to touch anything beyond this point
-    watch: {
-      css: {
-        files: 'stylesheets/!(ie).css',
-        tasks: ['csslint:css'],
-      },
-      jekyll: {
-        options: {
-          livereload: true,
-        },
-        files: ['_site/*'],
-      },
-    },
+    
   });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-jekyll');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-
-  // Default task.
-  grunt.registerTask('default', ['csslint:css', 'jekyll:build', 'watch']);
+  grunt.registerTask('default', []);
 
 };
