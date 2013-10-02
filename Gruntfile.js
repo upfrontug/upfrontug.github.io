858b8d73fef913ba86f132f81245292678213347
Max Fellmuth <max@maxfellmuth.com>
2013-10-02 23:54:11 +0200

    Add SASS-Stylesheets and SASS-Grunttasks, Close #2

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,43 +2,16 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    csslint: {
-      css: {
-        src: ['stylesheets/!(ie).css']
-      },
-      
-    },
-    jekyll: {
-      server: {
-        server : true,
-        server_port : 3000,
-        auto : true
-      },
-      build: {
-        server: false
-      }
-    },
-    //System Configuartion. You sdhouldn't need to touch anything beyond this point
-    watch: {
-      css: {
-        files: 'stylesheets/!(ie).css',
-        tasks: ['csslint:css'],
-      },
-      jekyll: {
-        options: {
-          livereload: true,
-        },
-        files: ['_site/*'],
-      },
-    },
+    sass: {
+            dist: {
+                files: {
+                    'stylesheets/custom.css': 'stylesheets/custom.scss'
+                }
+            }
+    }
   });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-jekyll');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-
-  // Default task.
-  grunt.registerTask('default', ['csslint:css', 'jekyll:build', 'watch']);
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.registerTask('default', []);
 
 };
